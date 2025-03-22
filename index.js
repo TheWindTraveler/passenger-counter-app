bf9d63ecebcff6bb7eba82ed0bc9037e866e958b
Begotten-Traveler <solomondev002@gmail.com>
2025-03-22 02:20:12 +0100

    add: the save and preious entries button and its styles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Change the ID in the HTML element which is called count-el to countEl
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count__el");
+let saveEl = document.getElementById("save__el")
 
 // Let the initial count be 0 at first
 let count = 0;
@@ -12,4 +13,17 @@ function increment (){
     countEl.innerText = count;
     // Display it in the console incase there is any error
     console.log(count);
+}
+
+// SAVE
+function save(){
+    // If the (COUNT) is greater than 0 then save it - this is because the user keeps saving 0 to the previous entries
+    if (count > 0) {
+        let saveStr = count + " - ";
+        // This saves the (COUNT)
+        saveEl.textContent += saveStr;
+        // This returns the (COUNT) back to 0
+        count = 0;
+        countEl.textContent = count;
+    }
 }
